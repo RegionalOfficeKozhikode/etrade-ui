7fbee097fb90c60153878e97426044801e9c1cc0
konzern <kv.nijesh@gmail.com>
2021-10-08 22:55:36 +0530

    login window created

---
+++ etrade.ui/src/main/java/com/etrade/config/EtradeConfig.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.etrade.config;
+import java.util.Properties;
+
+/**
+ * @author apple
+ *
+ */
+public class EtradeConfig {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+	}
+
+	
+	private void getPropertyValues() {
+		
+		Properties properties = new Properties();
+		
+	}
+}
---
+++ etrade.ui/src/main/java/com/etrade/ui/MyLauncher.java
@@ -0,0 +1,6 @@
+package com.etrade.ui;
+public class MyLauncher {
+    public static void main(String[] args){
+        EtradeLogin.main(args);
+    }
+}
